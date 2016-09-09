1d2da81b1a03c84ea8d276b13c6de372a58c82ba
markDeveloping <mark.developing@gmail.com>
2016-09-09 15:57:48 +0100

    fix heroku error cause.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-  	user = User.find_by(email: params[session:][:email].downcase)
-  	if user && user.authenticate(params[session:][:password])
-  	else
-  		flash[:danger] = "Invalid email or password combination."
-  	render 'new'
-  	end
-  end
+  #def create
+  #	user = User.find_by(email: params[session:][:email].downcase)
+  #	if user && user.authenticate(params[session:][:password])
+  #	else
+  #		flash[:danger] = "Invalid email or password combination."
+  #	render 'new'
+  #	end
+  #end
 
   def destroy
   end
