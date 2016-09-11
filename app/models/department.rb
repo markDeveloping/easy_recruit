d9b2d882a7ba5bb122f126de2e94dba13794ae38
markDeveloping <mark.developing@gmail.com>
2016-09-11 11:37:37 +0100

    Made department to user relationship and added additional validation to users model.

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,4 @@
 class Department < ActiveRecord::Base
 	has_many :jobs
+	has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-	validates :firstname, presence: true
-	validates :lastname, presence: true
-	validates :email, presence: true
+	belongs_to :department
+	before_save { email.downcase! }
+	validates :firstname, presence: true, length: { maximum: 50 }
+	validates :lastname, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, confirmation: true, format: { with: VALID_EMAIL_REGEX }
 	validates :email_confirmation, presence: true
 	has_secure_password
+	validates :password_digest, confirmation: true, length: { minimum: 6 }
 	validates :password_digest_confirmation, presence: true
-	validates :email, confirmation: true
-	validates :password_digest, confirmation: true
 end
