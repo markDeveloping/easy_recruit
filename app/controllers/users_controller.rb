65aa01462c5ff6103de151c003c80bf3cab00567
markDeveloping <mark.developing@gmail.com>
2016-09-16 23:15:04 +0100

    added department_id to create an account form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	
-	before_action :logged_in_user, only: [:edit, :update]
+	before_action :logged_in_user, only: [:show]
 
 	def show
 		@user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+  get 'static_pages/home'
+
+  get 'static/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
